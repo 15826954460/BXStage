3ea282f3cdd433846af215a5de7660853fc06ebb
15826954460 <624793604@qq,com>
2018-12-06 20:10:37 +0800

    一些常用的数据接口的存档

---
+++ app/pages/testView/stackStructure.js
@@ -0,0 +1,50 @@
+const stack = {
+  childRouters: [
+    {
+      ApplySuccess: null,
+      AuthStatus: null,
+      BigNoAuthority: null,
+      BigPurchaseSuccess: null,
+      BorrowerIndex: null,
+      BorrowerLogin: null,
+      CheckVerifyType: null,
+      CreditCard: null,
+      CreditCardInfo: null,
+      DiscountedClaims: null,
+      DiscountedDetail: null,
+      Faq: null,
+      LoanTransfer: null,
+      MyTransferList: null,
+      Notice: null,
+      Project: null,
+      ProjectData: null,
+      ProjectDetail: null,
+      ProjectList: null,
+      PurchaseConfirmation: null,
+      PurchaseFailure: null,
+      SmallNoAuthority: null,
+      SyncBill: null,
+      TransferTurnOut: null,
+      VerifyCode: null,
+    }
+  ],
+  getActionCreators: function getActionCreators(route, navStateKey) {
+  },
+  getActionForPathAndParams: function getActionForPathAndParams(path, params) {
+  },
+  getComponentForRouteName: function getComponentForRouteName(routeName) {
+
+  },
+  getComponentForState: function getComponentForState(state) {
+
+  },
+  getPathAndParamsForState: function getPathAndParamsForState(state) {
+
+  },
+  getScreenOptions: function getScreenOptions() {
+
+  },
+  getStateForAction: function getStateForAction(action, state) {
+
+  }
+}
--- ios/RNEleProject/AppDelegate.m
+++ ios/RNEleProject/AppDelegate.m
@@ -8,7 +8,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
-#import <React/RCTRootView.h>
+#import <React/RCTRootView.h>
 #import "RNSplashScreen.h"
 
 
